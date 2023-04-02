58db377e6b25fc6dce622d49fe69a15e0e8c7669
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-04-03 00:42:31 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    axios.get("https://randomuser.me/api/?results=10").then((response) => {
+      setUsers(response.data.results);
+    });
+    setShow(true);
+  }, [show]);
+
+  const userCount = users.length;
+  const radius = 130;
+  const center = { x: 250, y: 250 };
+  let angle = 0;
+
+  const changeCircle = () => {
+    setShow(false);
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="circle">
+       
+        {users.map((user) => {
+          
+          const x = center.x + radius * Math.cos((angle * Math.PI) / 180);
+          const y = center.y + radius * Math.sin((angle * Math.PI) / 180);
+          angle += 360 / userCount;
+          return (
+            <div key={user.login.uuid} className="user" style={{ left: x, top: y }}>
+              
+              <img src={user.picture.large} alt={user.name.first} className="avatar" />
+
+            </div>
+          );
+   
+        })}
+      </div>
+      <img className="avatar me" src="https://avatars.githubusercontent.com/u/95706081?v=4" alt="profile" />
+      <button onClick={changeCircle}>Change Circle</button>
     </div>
   );
 }
